4118bc886921420454c3e679a55c6a08f65aae09
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:09:07 +0530

    Update exp10c.R

--- DMDA/exp10c.R
+++ DMDA/exp10c.R
@@ -1,15 +1,17 @@
-data("iris")
-install.packages("caret")
-install.packages("C50")
-library(caret)
-library(C50)
-set.seed(7)
-inTraininglocal <- createDataPartition(iris$Species, p = .70, list = F)
-training <- iris[inTraininglocal,]
-testing <- iris[-inTraininglocal,]
-model <- C5.0(Species ~ ., data = training)
-summary(model)
-pred <- predict.C5.0(model, testing[,-5])
-a <- table(testing$Species, pred)
-sum(diag(a)) / sum(a)
-plot(model)
+# Sample Data: Sales vs Time
+time <- c(1, 2, 3, 4, 5) 
+sales <- c(50, 60, 75, 95, 120)  
+data <- data.frame(time, sales)
+library(rpart)
+tree_model <- rpart(sales ~ time, data = data, control = rpart.control(minsplit = 1, cp = 0))
+plot(tree_model, uniform = TRUE, main = "Decision Tree")
+text(tree_model, use.n = TRUE, cex = 0.8)
+predicted_sales <- predict(tree_model, newdata = data)
+plot(
+  time, sales, 
+  main = "Decision Tree Regression", 
+  xlab = "Time", 
+  ylab = "Sales", 
+  pch = 16, col = "blue"
+)
+lines(time, predicted_sales, col = "red", lwd = 2)  # Add predicted line
