26023375b082cd313e67f0555bed3614a8bc5918
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:39:55 +0530

    Update exp9linearregression.R

--- DMDA/exp9/exp9linearregression.R
+++ DMDA/exp9/exp9linearregression.R
@@ -1,11 +1,7 @@
-
-set.seed(123)
+#Predicting someone&#39;s weight based on their height.
+# Define height and weight vectors
 height <- c(150, 160, 170, 180, 190)
-weight <- c(50, 60, 65, 75, 85)
-linear_model <- lm(weight ~ height)
-summary(linear_model)
-ggplot(data = data.frame(height, weight), aes(x = height, y = weight)) +
-  geom_point() +
-  geom_smooth(method = "lm", color = "blue", se = FALSE) +
-  ggtitle("Linear Regression: Weight vs Height") +
-  theme_minimal()
+weight <- c(50, 60, 70, 80, 90)
+model <- lm(weight ~ height)
+summary(model)
+predict(model, newdata = data.frame(height = c(165, 175)))
