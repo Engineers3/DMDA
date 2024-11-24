f9c3f1bec06569fca8fddfab48f83b9077a0e94e
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:44:26 +0530

    Update exp9linearregression.R

--- DMDA/exp9/exp9linearregression.R
+++ DMDA/exp9/exp9linearregression.R
@@ -1,7 +1,7 @@
-#Predicting someone&#39;s weight based on their height.
-# Define height and weight vectors
+# Define the height and weight data
 height <- c(150, 160, 170, 180, 190)
 weight <- c(50, 60, 70, 80, 90)
 model <- lm(weight ~ height)
-summary(model)
-predict(model, newdata = data.frame(height = c(165, 175)))
+plot(height, weight, main = "Height vs. Weight",
+     xlab = "Height (cm)", ylab = "Weight (kg)", pch = 16)
+abline(model, col = "red")
