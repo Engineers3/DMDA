fde127b69b6936e5031a862e02b807641990ceb4
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:16:28 +0530

    Update exp12c.R

--- DMDA/exp12/exp12c.R
+++ DMDA/exp12/exp12c.R
@@ -1,7 +1,9 @@
-install.packages("randomForest")
-library(party)
-print(head(readingSkills))
-library(party)
-library(randomForest)
-output.forest <- randomForest(nativeSpeaker ~ age + shoeSize + score, data = readingSkills)
-print(output.forest) 
+# Step 1: Create the test scores for both groups
+group1 <- c(78, 85, 91, 88, 84, 79, 95)
+group2 <- c(72, 74, 69, 75, 70, 68, 74)  
+boxplot(group1, group2, names = c("Tutored", "Non-Tutored"), 
+        main = "Test Scores Comparison", 
+        ylab = "Test Scores", 
+        col = c("lightblue", "lightgreen"))
+t_test_result <- t.test(group1, group2, alternative = "two.sided", var.equal = TRUE)
+print(t_test_result)
