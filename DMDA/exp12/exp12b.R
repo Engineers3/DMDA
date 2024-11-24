59d2fe12a644825c6ece662fbd7e35f98c4d0893
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:14:05 +0530

    Update exp12b.R

--- DMDA/exp12/exp12b.R
+++ DMDA/exp12/exp12b.R
@@ -1,3 +1,12 @@
-x  <- c(0.593, 0.142, 0.329, 0.691, 0.231, 0.793, 0.519, 0.392, 0.418)
-t.test(x, alternative="greater", mu=0.3)
- 
+# Step 1: Create the data for each department
+
+department_A <- c(50, 52, 55, 49, 51)
+department_B <- c(60, 62, 64, 58, 61)
+department_C <- c(45, 47, 48, 43, 44)
+department <- factor(c(rep("A", length(department_A)),
+                       rep("B", length(department_B)),
+                       rep("C", length(department_C))))
+income <- c(department_A, department_B, department_C)
+data <- data.frame(department, income)
+anova_result <- aov(income ~ department, data = data)
+summary(anova_result)
