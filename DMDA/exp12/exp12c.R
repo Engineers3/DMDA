e69bc03664b55ad4197d83575fa874cc4872e68f
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 20:13:32 +0530

    Update exp12c.R

--- DMDA/exp12/exp12c.R
+++ DMDA/exp12/exp12c.R
@@ -1,9 +1,12 @@
-# Step 1: Create the test scores for both groups
-group1 <- c(78, 85, 91, 88, 84, 79, 95)
-group2 <- c(72, 74, 69, 75, 70, 68, 74)  
-boxplot(group1, group2, names = c("Tutored", "Non-Tutored"), 
-        main = "Test Scores Comparison", 
-        ylab = "Test Scores", 
-        col = c("lightblue", "lightgreen"))
-t_test_result <- t.test(group1, group2, alternative = "two.sided", var.equal = TRUE)
-print(t_test_result)
+# Step 1: Create the data for each department
+
+department_A <- c(50, 52, 55, 49, 51)
+department_B <- c(60, 62, 64, 58, 61)
+department_C <- c(45, 47, 48, 43, 44)
+department <- factor(c(rep("A", length(department_A)),
+                       rep("B", length(department_B)),
+                       rep("C", length(department_C))))
+income <- c(department_A, department_B, department_C)
+data <- data.frame(department, income)
+anova_result <- aov(income ~ department, data = data)
+summary(anova_result)
