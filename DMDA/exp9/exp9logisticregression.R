35aabda13573bf0b21a7969f2e5085a9e5179964
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 17:41:51 +0530

    Update exp9logisticregression.R

--- DMDA/exp9/exp9logisticregression.R
+++ DMDA/exp9/exp9logisticregression.R
@@ -1,12 +1,6 @@
-
-set.seed(123)
-overweight <- ifelse(height > 165, 1, 0) 
-height_logistic <- height
-logistic_model <- glm(overweight ~ height_logistic, family = binomial)
-summary(logistic_model)
-library(ggplot2)
-ggplot(data = data.frame(height_logistic, overweight), aes(x = height_logistic, y = overweight)) +
-  geom_point(aes(color = as.factor(overweight))) +
-  geom_smooth(method = "glm", method.args = list(family = "binomial"), color = "blue") +
-  ggtitle("Logistic Regression: Overweight vs Height") +
-  theme_minimal()
+# Define age and buy vectors
+age <- c(20, 30, 40, 50, 60)
+buy <- c(0, 1, 0, 1, 1) 
+model <- glm(buy ~ age, family = binomial)
+summary(model)
+predict(model, newdata = data.frame(age = c(25, 35)), type = "response")
