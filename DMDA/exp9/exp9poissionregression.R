01b9cb83916ecf9b5209b38c27c917628ebbd3ce
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 16:20:01 +0530

    Update exp9poissionregression.R

--- DMDA/exp9/exp9poissionregression.R
+++ DMDA/exp9/exp9poissionregression.R
@@ -1,8 +1,10 @@
-input <- warpbreaks
-print(head(input))
-output <-glm(formula = breaks ~ wool+tension, data = warpbreaks,
-             family = poisson)
-print(summary(output))
-input <- warpbreaks
-output <- glm(formula = breaks ~ wool + tension, data = warpbreaks, family = poisson)
-print(summary(output))
+
+set.seed(123)
+exercises <- rpois(5, lambda = exp(0.05 * height)) 
+summary(poisson_model)
+library(ggplot2)
+ggplot(data = data.frame(height, exercises), aes(x = height, y = exercises)) +
+  geom_point(aes(color = exercises)) +
+  geom_smooth(method = "glm", method.args = list(family = "poisson"), color = "blue") +
+  ggtitle("Poisson Regression: Exercises per Week vs Height") +
+  theme_minimal()
