3feeacae0661b7fe5f83d3133c64e7fc01fe130c
Engineers3 <sravankumar.200315@gmail.com>
2024-11-24 18:12:16 +0530

    Update exp10c.R

--- DMDA/exp10c.R
+++ DMDA/exp10c.R
@@ -1,13 +1,16 @@
 
+data("iris")
+install.packages("caret")
+install.packages("C50")
 library(caret)
 library(C50)
 set.seed(7)
 inTraininglocal <- createDataPartition(iris$Species, p = .70, list = FALSE)
-training <- iris[inTraininglocal,]
-testing <- iris[-inTraininglocal,]
+training <- iris[inTraininglocal, ]
+testing <- iris[-inTraininglocal, ]
 model <- C5.0(Species ~ ., data = training)
 summary(model)
-pred <- predict.C5.0(model, testing[,-5])
+pred <- predict(model, testing[, -5])  # use predict() instead of predict.C5.0()
 a <- table(testing$Species, pred)
 accuracy <- sum(diag(a)) / sum(a)
 print(accuracy)
